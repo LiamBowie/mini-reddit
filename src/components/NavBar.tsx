2bed7cf776f2d8d187aa93c5bf504f38770e359f
Liam Bowie <liambowie16@gmail.com>
2023-02-10 17:24:31 +0000

    Implement responsive design on the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,9 +6,9 @@ function NavBar() {
 
     return (
         <div className="fixed top-0 left-0 right-0 h-10 flex justify-between items-center py-7 px-6 border border-black ">
-            <img src={RedditLogo} className="h-10 w-10 p-1" />
+            <img src={RedditLogo} className="h-10 w-10 py-1 mr-4" />
             <SearchBar />
-            <HiBookmark className="w-10 h-10 text-gray-400 hover:text-gray-600" />
+            <HiBookmark className="w-10 h-10 text-gray-400 hover:text-gray-600 hidden sm:block" />
         </div>
     )
 }
